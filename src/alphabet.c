146d77e0218810b11b62114ccf4d09d07ce8b2fa
pratas <pratas@ua.pt>
2017-07-11 11:55:15 +0100

    alphabet

---
+++ src/alphabet.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "mem.h"
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// CREATE ALPHABET
+//
+ALPHABET *CreateAlphabet(void){
+  ALPHABET *A    = (ALPHABET *) Calloc(1,                 sizeof(ALPHABET));
+  A->numeric     = (uint8_t  *) Calloc(ALPHABET_MAX_SIZE, sizeof(uint8_t));
+  A->toChars     = (uint8_t  *) Calloc(ALPHABET_MAX_SIZE, sizeof(uint8_t));
+  A->revMap      = (uint8_t  *) Calloc(ALPHABET_MAX_SIZE, sizeof(uint8_t));
+  A->alphabet    = (uint8_t  *) Calloc(ALPHABET_MAX_SIZE, sizeof(uint8_t));
+  A->mask        = (uint8_t  *) Calloc(ALPHABET_MAX_SIZE, sizeof(uint8_t));
+  A->cardinality = 0;
+  return A;
+  }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// LOAD ALPHABET
+//
+void LoadAlphabet(ALPHABET *A, FILE *F){
+  uint32_t x;
+  int32_t  k;
+  uint8_t  *buffer;
+
+  buffer = (uint8_t *) Calloc(BUFFER_SIZE, sizeof(uint8_t));
+  while((k = fread(buffer, 1, BUFFER_SIZE, F)))
+    for(x = 0 ; x < k ; ++x)
+      A->mask[buffer[x]] = 1;
+  Free(buffer);
+
+  A->cardinality = 0;
+  for(x = 0 ; x < ALPHABET_MAX_SIZE ; x++){
+    if(A->mask[x]){
+      A->toChars[A->cardinality] = x;
+      seq->revMap[x] = A->cardinality++;
+      }      
+    else 
+      seq->revMap[x] = INVALID_SYMBOL;
+    }
+
+  rewind(F);
+  }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// REMOVE ALPHABET
+//
+void RemoveAlphabet(ALPHABET *A){
+  Free(A->numeric);
+  Free(A->toChars);
+  Free(A->revMap);
+  Free(A->alphabet);
+  Free(A->mask);
+  Free(A);
+  }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
---
+++ src/alphabet.h
@@ -0,0 +1,29 @@
+#ifndef ALPHABET_H_INCLUDED
+#define ALPHABET_H_INCLUDED
+
+#include "defs.h"
+
+#define ALPHABET_MAX_SIZE 256
+#define INVALID_SYMBOL    255
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+typedef struct {
+  uint8_t    *numeric;    // Symbols in numbers (20213413...) from file
+  uint8_t    *toChars;    // chars: { 'A', 'C', 'G', 'T', 'N', ...}
+  uint8_t    *revMap;     // Reverse symbols to numbers
+  uint8_t    *alphabet;
+  uint8_t    *mask;       // binary vector with symbol existing or not
+  int        cardinality;
+  }
+ALPHABET;
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+void    CreateAlphabet   (void);
+void    LoadAlphabet     (ALPHABET *, FILE *);
+void    RemoveAlphabet   (ALPHABET *);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+#endif
